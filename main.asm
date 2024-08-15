2e9e7be277386fc18d9312aab6bd0e313e152151
Adam Ainsworth <adam@studioillicit.com>
2024-08-15 08:27:49 +0100

    Start of relocate routine. Seems to work but breaks ROM calls

--- main.asm
+++ main.asm
@@ -1,4 +1,4 @@
-        org	$fd00
+        org	$0000
 
 Main:
         push hl
@@ -17,5 +17,41 @@ Exit:
 
         ret
 
+        ; $81d4 / 33236
+Relocate:
+        ld de, $0000 ; this needs to be set by the hacker to the memory address of Main
+        ld hl, de ; HL will be the memory counter to work through the code
+        ld bc, $0200 ; length of our program
+RelocateLoop:
+        ld a, (hl)
+        cp $cd ; is it a call?
+        jr z, RelocateUpdate ; if so, update the following bytes
+RelocateCont:
+        inc hl
+        dec bc
+        ld a, b
+        or c
+        jr nz, RelocateLoop
+        ret ; all done - check before running!
+RelocateUpdate:
+        inc hl
+        ld a, (hl)
+        add e
+        ld (hl), a
+        jr nc, RelocateNC
+        inc hl
+        inc (hl)
+        jr RelocateC
+RelocateNC:
+        inc hl
+RelocateC:
+        ld a, (hl)
+        add d
+        ld (hl), a
+
+        dec bc
+        dec bc
+        jr RelocateCont
+
 Address:
-        defb $00, $fd
+        defb $00, $80
