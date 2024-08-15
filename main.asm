061ae93791332a48e5c862a4ab640bb6277d5ac2
Adam Ainsworth <adam@studioillicit.com>
2024-08-15 16:31:41 +0100

    Minor optimisation

--- main.asm
+++ main.asm
@@ -1,23 +1,43 @@
         org	$1000
         define _ADDRESS $fefe
 Main:
-        push hl
-
 Loop:
         call UpdateView
-        call UserInput
+        call WaitKey
+
+        cp $58 ; exit
+        ret z
+        
+        cp $20 ; change address
+        call z, ChangeAddress
+        cp $0d ; poke address
+        call z, PokeAddress
+
+        cp $09 ; forward by 1
+        call z, ForwardAddr
+        cp $08 ; back by 1
+        call z, BackAddr
+
+        cp $0a ; forward by 16
+        call z, ForwardLine
+        cp $0b ; back by 16
+        call z, BackLine
+
+        cp $50 ; forward by 256
+        call z, ForwardPage
+        cp $4f ; back by 256
+        call z, BackPage
+
+        cp $54 ; toggle text
+        call z, TextToggle
+
         jr Loop
 
         INCLUDE "output.asm"
         INCLUDE "commands.asm"
         INCLUDE "input.asm"
 
-Exit:
-        pop hl
-
-        ret
-
-        ; $01d4
+        ; $01cc / 460
 Relocate:
         call $0013 ; call a ret
         dec sp
@@ -25,19 +45,17 @@ Relocate:
         ex (sp), hl ; grab the old return address - we now have a PC
         inc sp
         inc sp
-        ld de, $FE29
+        ld de, $FE31
         add hl, de ; pull it back to the beginning of the program
                    ; HL will be the memory counter to work through the code
         ld a, h
         sub $10
         ld d, a
         ld e, l ; DE will be the base to add to each address (ie. the address of Start)
-        ld bc, $01d4 ; length of our program
+        ld bc, $01cc ; length of our program
 RelocateLoop:
         ld a, (hl)
         cp $cd ; is it a call?
-        jr z, RelocateUpdate ; if so, update the following bytes
-        cp $cc ; is it a call?
         jr z, RelocateUpdate ; if so, update the following bytes
 RelocateCont:
         inc hl
